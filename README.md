08fb6e6e4594554121e95e33e47906488f5dc8b7
sstraw <sean.r.straw@gmail.com>
2018-11-23 12:23:46 -0800

    Added decoding support and checking for pipe on input

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Convert
 A collection of tools for converting between various formats written in Go
+
+##csv2xlsx
+Reads a csv and writes it to an xlsx. Tries to detect utf-16 as well.
+```
+$ ./csv2xlsx -h
+Usage of ./csv2xlsx:
+  -f    Force - overwrites existing files
+  -i string
+        File to read from. If a pipe is detected,
+        reads from stdin. Use - to explicitly read
+        from stdin.
+  -o string
+        File to write out to. Use - to write to stdout
+```
--- cmd/csv2xlsx/csv2xlsx.go
+++ cmd/csv2xlsx/csv2xlsx.go
@@ -6,6 +6,7 @@ import (
     "log"
     "encoding/csv"
     "flag"
+    "bufio"
 
     "golang.org/x/text/encoding/unicode"
     "golang.org/x/text/transform"
@@ -14,34 +15,67 @@ import (
 
 func main() {
 
-    finPtr := flag.String("i", "", "File to read from.")
-    foutPtr := flag.String("o", "", "File to write out to.")
+    finPtr := flag.String("i", "", "File to read from. If a pipe is detected,\n" +
+                          "reads from stdin. Use - to explicitly read\n" +
+                          "from stdin.")
+
+    foutPtr := flag.String("o", "", "File to write out to. Use - to write to stdout")
+    force := flag.Bool("f", false, "Force - overwrites existing files")
 
     flag.Parse()
 
-    if *finPtr == "" {
-        flag.PrintDefaults()
-        log.Fatal("-i required")
-    } else if *foutPtr == "" {
-        flag.PrintDefaults()
-        log.Fatal("-o required")
+    if *foutPtr == "" {
+        log.Fatal("-o is required")
     }
 
-    file, err := os.Open(*finPtr)
-    if err != nil {
-        log.Fatal(err)
+    //File input opening
+    var fin *(os.File)
+    var err error
+    if *finPtr == "-" {
+        fin = os.Stdin
+        log.Println("Reading from stdin")
+    } else if *finPtr != ""{
+        fin, err = os.Open(*finPtr)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer fin.Close()
+    } else {
+        stat, _ := os.Stdin.Stat()
+        if (stat.Mode() & os.ModeCharDevice) == 0 {
+            //Checks stickychar to see if data is being piped
+            fin = os.Stdin
+        } else {
+            log.Println("No pipe detected and no input file specified.")
+            log.Fatal("Use -i - to force reading from stdin")
+        }
     }
-    defer file.Close()
 
-    fout, err := os.Create(*foutPtr)
+    //File output opening
+    var fout *(os.File)
+    if *foutPtr == "-" {
+        fout, err = os.Stdout, nil
+    } else if *force {
+        fout, err = os.OpenFile(*foutPtr, os.O_RDWR | os.O_CREATE, 0666)
+    } else {
+        fout, err = os.OpenFile(*foutPtr, os.O_RDWR | os.O_CREATE | os.O_EXCL, 0666)
+    }
+
     if err != nil {
         log.Fatal(err)
     }
     defer fout.Close()
 
-    utf16lebom := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewDecoder()
 
-    csvReader := csv.NewReader(transform.NewReader(file, utf16lebom))
+    //Input reading
+    enc, err:= EncHandle(fin)
+    if err == io.EOF {
+        log.Fatal("Reached EOF - Not enough data")
+    } else if err != nil {
+        log.Fatal(err)
+    }
+
+    csvReader := csv.NewReader(enc)
 
     xlsxFile := xlsx.NewFile()
     xlsxSheet, err := xlsxFile.AddSheet("Sheet")
@@ -64,4 +98,40 @@ func main() {
     if err != nil {
         log.Println(err)
     }
+}
+
+func EncHandle (fhandle io.Reader) (io.Reader, error) {
+    /*
+    Attempts to detect encoding (specifically, decide if
+    utf-16le, utf-16be or utf-8 with or without bom)
+    and returns a reader object for it, translating 
+    if necessary
+    */
+    buf := bufio.NewReader(fhandle)
+
+    head, err := buf.Peek(4)
+
+    if err!= nil {
+        return nil, err
+    }
+
+    switch {
+        //UTF-16-le-like with no bom
+        case head[0] != 0x00 && head[1] == 0x00:
+            log.Println("Assuming utf-16-LE")
+            fallthrough
+        //UTF-16-le with bom
+        case head[0] == 0xFF && head[1] == 0xFE:
+            dec := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewDecoder()
+            return transform.NewReader(buf, dec), nil
+        //UTF-16-be-like with no bom
+        case head[0] != 0x00 && head[1] == 0x00:
+            log.Println("Assuming utf-16-LE")
+            fallthrough
+        //UTF-16-be with bom
+        case head[0] == 0xFE && head[1] == 0xFF:
+            dec := unicode.UTF16(unicode.BigEndian,    unicode.UseBOM).NewDecoder()
+            return transform.NewReader(buf, dec), nil
+   }
+    return buf, nil
 }
